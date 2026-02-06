e64b2951c94db84a0f90c864ca77c3f0a3e73eb3
kennethwhitley1996-alt <kennethwhitley1996@gmail.com>
2026-02-06 04:20:20 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "My Love",
+    valentineName: "Daidel",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
